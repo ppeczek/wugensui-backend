ec38ad9823a5a93cec460f0141d6da66e627714b
Piotr Peczek <ppeczek@gmail.com>
2018-03-13 14:28:23 +0100

    provided corner points and states of processing

--- wugensui/models.py
+++ wugensui/models.py
@@ -1,10 +1,22 @@
 from mongoengine import Document, fields
 
 
+NOT_PROCESSED, CREATING_PATTERN, PROCESSING_VIDEO, SAVING_RESULTS, FINISHED = range(1, 6)
+
+PROCESSING_STATES = (
+    (NOT_PROCESSED, 'not processed'),
+    (CREATING_PATTERN, 'creating pattern'),
+    (PROCESSING_VIDEO, 'processing video'),
+    (SAVING_RESULTS, 'saving results'),
+    (FINISHED, 'finished')
+)
+
+
 class Video(Document):
     name = fields.StringField(required=True, unique=True)
     video_path = fields.StringField(verbose_name='video', required=False)
-    # state = fields.
+    state = fields.IntField(choices=PROCESSING_STATES)
+    corners = fields.MultiPointField()
     # pattern = fields.ImageField(upload_to='results', required=False)
     # pattern_frames = fields.ListField(fields.ImageField(upload_to='results'))
 
